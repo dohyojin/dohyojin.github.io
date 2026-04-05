b15a8b7e1127ada9e25f0971fa077b0b035cea32
dohyojin <dohyojin90@gmail.com>
2026-04-05 06:35:02 +0000

    Deploying to gh-pages from @ dohyojin/dohyojin.github.io@393154fee9e2f89e3fdd4f480c59b62c272fc859 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,51 +28,31 @@ ninja.data = [{
           section: "News",},{id: "news-i-ll-be-at-uist-in-busan-south-korea-9-28-10-1-and-aies-in-madrid-spain-10-20-22-️",
           title: 'I’ll be at UIST in Busan, South Korea (9/28-10/1) and AIES in Madrid,...',
           description: "",
-          section: "News",},{id: "projects-llm-explainability",
-          title: 'LLM Explainability',
-          description: "Exploring methods for explaining factuality and attributions of LLM outputs",
+          section: "News",},{id: "projects-ai-disclosure",
+          title: 'AI Disclosure',
+          description: "Exploring designs of AI disclosure, informing consumers whether and how AI-generated content are produced.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/disclosure/";
             },},{id: "projects-llm-as-a-judge",
           title: 'LLM-as-a-Judge',
           description: "Designing and building LLM-based evaluation tool for human-AI alignment",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-facilitator-agent",
-          title: 'Facilitator Agent',
-          description: "Designing conversational agents for facilitating online group discussions",
+              window.location.href = "/projects/evalassist/";
+            },},{id: "projects-explainability",
+          title: 'Explainability',
+          description: "Exploring methods for explaining factuality and source attribution of language model outputs",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-ai-disclosure",
-          title: 'AI Disclosure',
-          description: "Understanding governance practices and designing tools",
+              window.location.href = "/projects/explainability/";
+            },},{id: "projects-ai-governance",
+          title: 'AI Governance',
+          description: "Understanding Industry Practitioners&#39; Experiences in Generative AI Governance",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/governance/";
+            },},{id: "projects-conversational-interface",
+          title: 'Conversational Interface',
+          description: "Designing conversational interfaces for a workflow automation system.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/grounding/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
