1bab337601488959c9d46214b1263c7b10779503
dohyojin <dohyojin90@gmail.com>
2026-08-25 14:53:34 +0000

    Deploying to gh-pages from @ dohyojin/dohyojin.github.io@20e521132efe0ee5903fea4c7f7e55aef38d3960 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-research",
           title: "Research",
-          description: "Human-centered AI research",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
