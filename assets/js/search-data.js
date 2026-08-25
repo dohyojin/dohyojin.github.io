47d2f465ae91a5dd6617094abdf4532daa6527f9
dohyojin <dohyojin90@gmail.com>
2026-08-25 13:41:56 +0000

    Deploying to gh-pages from @ dohyojin/dohyojin.github.io@7746ea6efb30d725bceb62898e7c71164c434b07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
         },{id: "news-announcement-1",
           title: 'Announcement_1',
           description: "",
